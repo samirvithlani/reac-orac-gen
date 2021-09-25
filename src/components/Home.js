5ac9ef156ecb9463a86bc6ac8a390ac3b2db37fe
=samirvithlani <=samir.vithlani83955@gmail.com>
2021-09-25 11:23:24 +0530

    Redux update

--- src/components/Home.js
+++ src/components/Home.js
@@ -18,6 +18,15 @@ export const Home = (props) => {
                     <button onClick={()=>{props.removeToCartHandler({price:12000,name:"IPHONE"})}}>REMOVE TO CART</button>
                 </div>
             </div>
+            <div class="card" style={style}>
+                <img class="card-img-top" src="https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-12-pro-family-hero?wid=940&hei=1112&fmt=jpeg&qlt=80&.v=1604021663000" alt="Card image cap" />
+                <div class="card-body">
+                    <h5 class="card-title">IPHONE13</h5>
+                    <h5 class="card-title">PRICE: 225000</h5>
+                    <button onClick={()=>{props.addToCartHandler({price:15000,name:"IPHONE13"})}}>ADD TO CART</button>
+                    <button onClick={()=>{props.removeToCartHandler({price:15000,name:"IPHONE13"})}}>REMOVE TO CART</button>
+                </div>
+            </div>
         </div>
 
     )
